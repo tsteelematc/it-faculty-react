5ea2d9dd2bf1df094673ba199df712631723cabe
Tom Steele <tsteele@madisoncollege.edu>
2020-07-05 00:48:06 -0500

    Updates

---
+++ precache-manifest.2dc1f1508d7f286d4b8b1e8a7f523658.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8392a50fb4fa8c9ff8f0347c6cb73fa",
+    "url": "/it-faculty-react/index.html"
+  },
+  {
+    "revision": "a783b117f446163b8aee",
+    "url": "/it-faculty-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "12d8d6a6049e5f4886ef",
+    "url": "/it-faculty-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a783b117f446163b8aee",
+    "url": "/it-faculty-react/static/js/2.347b1299.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/it-faculty-react/static/js/2.347b1299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d8d6a6049e5f4886ef",
+    "url": "/it-faculty-react/static/js/main.5db251a4.chunk.js"
+  },
+  {
+    "revision": "e17d906f1abdaa213cf0",
+    "url": "/it-faculty-react/static/js/runtime-main.caf9af70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/it-faculty-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
